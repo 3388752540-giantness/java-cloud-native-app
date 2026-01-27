e9f7d9c5b799771ff98f0c291868d5ed642665d5
3388752540 <3388752540ding@gmail.com>
2026-01-27 21:49:37 +0800

    test

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Dashboard from '../views/Dashboard.vue';
 import Register from '../views/Register.vue'; // 引入新页面
 import Careers from '../views/Careers.vue';//作业删除
+import Marketplace from '../views/Marketplace.vue';
 
 
 const routes = [
   { path: '/', component: Dashboard },
   { path: '/register', component: Register }, // 添加路径
-  { path: '/careers', component: Careers }
+  { path: '/careers', component: Careers },
+  { path: '/mall', component: Marketplace }
 ];
 
 const router = createRouter({
