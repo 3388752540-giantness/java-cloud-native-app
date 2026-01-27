e35e06a7e9df55ff6cef7f6bbe47733190703dc5
3388752540 <3388752540ding@gmail.com>
2026-01-27 23:44:42 +0800

    fix: 修复镜像拉取限制及前端路径大小写问题

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Dashboard from '../views/Dashboard.vue';
 import Register from '../views/Register.vue'; // 引入新页面
-import Careers from '../views/Careers.vue';//作业删除
-import Marketplace from '../views/Marketplace.vue';
+
 
 
 const routes = [
   { path: '/', component: Dashboard },
-  { path: '/register', component: Register }, // 添加路径
-  { path: '/careers', component: Careers },
-  { path: '/mall', component: Marketplace }
+  { path: '/register', component: Register }
 ];
 
 const router = createRouter({
